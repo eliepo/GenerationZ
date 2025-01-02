4496b5e539793c698918f2cb6a3c589f6fab0996
eli.epo@outlook.com <eli.epo@outlook.com>
2025-01-01 22:17:12 -0300

    Primeiro Commit

---
+++ src/main/java/com/GenerationZ/GenerationZApplication.java
@@ -0,0 +1,13 @@
+package com.GenerationZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationZApplication.class, args);
+	}
+
+}
