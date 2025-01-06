4249265c3977eaa1b6fa457a5953332ead6176c6
eli.epo@outlook.com <eli.epo@outlook.com>
2025-01-06 16:57:29 -0300

    Criação da inserção com usuario com Post

--- src/main/java/com/GenerationZ/services/UserService.java
+++ src/main/java/com/GenerationZ/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.GenerationZ.domain.User;
+import com.GenerationZ.dto.UserDTO;
 import com.GenerationZ.repository.UserRepository;
 import com.GenerationZ.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
+	}
 }
